2641b03af16c36740cf7b5d44180cf76dbc7b0ac
jprix <parisijason@gmail.com>
2023-11-10 14:47:22 -0500

    initial commit

---
+++ pages/_app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../theme';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
---
+++ theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#037FFF',
+    },
+
+    secondary: {
+      main: '#F7C366',
+    },
+    tertiary: {
+      main: '#008F7E',
+    },
+  },
+});
+
+export default theme;
